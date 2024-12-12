55552cf74c4aee7c0cd49f51029f114faa0255c6
Sandra <spadacinisandra@gmail.com>
2024-12-12 14:34:04 +0100

    List of Mobile

--- src/Controller/ApiMobileController.php
+++ src/Controller/ApiMobileController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\MobileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiMobileController extends AbstractController
 {
-    #[Route('/api/mobiles', name: 'app_api_mobile')]
-    public function getAllMobiles(): JsonResponse
+    #[Route('/api/mobiles', name: 'app_api_mobile', methods: ['GET'])]
+    public function getAllMobiles(MobileRepository $mobileRepository, SerializerInterface $serializer): JsonResponse
     {
-        return new JsonResponse([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ApiMobileController.php',
-        ]);
+        $mobileList = $mobileRepository->findAll();
+
+        $jsonMobileList = $serializer->serialize($mobileList, 'json');
+
+        return new JsonResponse($jsonMobileList, Response::HTTP_OK, [], true);
     }
 }
