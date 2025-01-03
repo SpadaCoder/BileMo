68e882bb7bc27cc0a7f111e67e4f465465f0b5e3
Sandra <spadacinisandra@gmail.com>
2025-01-03 11:48:15 +0100

    Update Voter

--- src/Security/Voter/UserVoter.php
+++ src/Security/Voter/UserVoter.php
@@ -3,22 +3,21 @@
 namespace App\Security\Voter;
 
 use App\Entity\AppUser;
-use App\Entity\Customer;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 final class UserVoter extends Voter
 {
-    public const DELETE = 'USER_DELETE';
-    public const VIEW = 'USER_VIEW';
-    public const CREATE = 'USER_CREATE';
+    public const APPUSER_DELETE = 'AppUserDelete';
+    public const APPUSER_VIEW = 'AppUserView';
+    // public const APPUSER_CREATE = 'AppUserCreate';
 
     protected function supports(string $attribute, mixed $subject): bool
     {
         // Vérifie que l'attribut est géré par ce voter
-        return in_array($attribute, [self::DELETE, self::VIEW, self::CREATE])
-            && ($subject instanceof AppUser || $subject instanceof Customer);
+        return in_array($attribute, [self::APPUSER_DELETE, self::APPUSER_VIEW])
+            && $subject instanceof AppUser;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
@@ -31,25 +30,25 @@ final class UserVoter extends Voter
         }
 
         switch ($attribute) {
-            case self::DELETE:
-                // Vérifie si le Customer peut supprimer un AppUser
+            case self::APPUSER_DELETE:
+            case self::APPUSER_VIEW:
                 if ($subject instanceof AppUser) {
-                    return $subject->getCustomer() === $customer;
+                    return $this->isOwner($subject, $customer);
                 }
                 break;
 
-            case self::VIEW:
-                // Vérifie si le Customer peut voir un AppUser
-                if ($subject instanceof AppUser) {
-                    return $subject->getCustomer() === $customer;
-                }
-                break;
-
-            case self::CREATE:
-                // Tous les Customers peuvent créer des utilisateurs
-                return true;
+            // case self::APPUSER_CREATE:
+            //     // Tous les Customers peuvent créer des utilisateurs
+            //     return true;
         }
 
         return false;
+
+    }
+
+    private function isOwner(AppUser $appUser, UserInterface $customer): bool
+    {
+        // Retourne vrai si le Customer est propriétaire de l'AppUser
+        return $appUser->getCustomer() === $customer;
     }
 }
